c2ddbbf97cb79a827bcd5537556a7ee5e65bf139
Vladimirov Denis <akakiy813@yandex.ru>
2019-11-05 10:11:05 +0300

    fix errors load cards

--- public/script.js
+++ public/script.js
@@ -19,7 +19,47 @@ fetch('https://praktikum.tk/cohort0/users/me', {
     console.log('Ошибка. Запрос не выполнен');
   });
 
-let initialCards = [];
+let initialCards = [{
+  name: 'Архыз',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+},
+{
+  name: 'Челябинская область',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+},
+{
+  name: 'Иваново',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+},
+{
+  name: 'Камчатка',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+},
+{
+  name: 'Холмогорский район',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+},
+{
+  name: 'Байкал',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+},
+{
+  name: 'Нургуш',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg'
+},
+{
+  name: 'Тулиновка',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg'
+},
+{
+  name: 'Остров Желтухина',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg'
+},
+{
+  name: 'Владивосток',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg'
+}];
+
 fetch('https://praktikum.tk/cohort0/cards', {
   headers: {
     authorization: '77cc3d84-97a1-47d6-8548-cefc66c159d6',
